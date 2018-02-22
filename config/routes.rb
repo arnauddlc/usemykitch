bc1756ccaa7fab04c8a70eaac1aac5eebc85394a
Mirha Masala <mirha.masala@me.com>
2018-02-22 10:59:53 +0800

    Create kitchens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'kitchens/index'
+
+  get 'kitchens/new'
+
+  get 'kitchens/show'
+
+  get 'kitchens/edit'
+
   devise_for :users
-  root to: 'pages#home'
+  root to: 'kitchens#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :kitchens
 end
