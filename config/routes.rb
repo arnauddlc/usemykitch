c4b5485437fad26f0020b3e0c8ac32682872d22d
Freddy Kurniawan <freddykwan.work@gmail.com>
2018-02-22 14:54:12 +0800

    create routes for bookings final

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  patch "/bookings/:id/decline", to: "bookings#decline", as: :decline_booking
-  patch "/bookings/:id/approve", to: "bookings#approve", as: :approve_booking
+  resources :bookings, only: [:destroy] do
+    member do
+      patch 'decline'
+      patch 'approve'
+    end
+  end
+
+  # patch "/bookings/:id/decline", to: "bookings#decline", as: :decline_booking
+  # patch "/bookings/:id/approve", to: "bookings#approve", as: :approve_booking
 end
