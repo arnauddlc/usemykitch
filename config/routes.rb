5ca3aaaf6d09ee00c83edebaed0201eca176e8a2
Freddy Kurniawan <freddykwan.work@gmail.com>
2018-02-22 15:10:40 +0800

    create controller for booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+
   devise_for :users
   root to: 'kitchens#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
