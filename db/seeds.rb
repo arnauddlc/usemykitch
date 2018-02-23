e57863e53afb6c4831bea6513771ef146699aa61
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-02-23 16:37:46 +0800

    updated seed to have nice pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,17 +17,18 @@ require 'faker'
 end
 puts "5 users created and saved!"
 
+
+i = 1
 8.times do
-	i = 1
 	image_path = "http://res.cloudinary.com/arnauddlc/image/upload/v1519371092/kitchens/kitchen#{i}.jpg"
-	puts image_url
+	puts image_path
 	price = rand(10..150)
 	user = User.find(rand(1..5))
-	title = Faker::StarWars.character
+	title = Faker::StarWars.character + "'s kitchen"
 	description ="Lorem ipsum dolor amet leggings taiyaki jean shorts pabst actually authentic vape green juice PBR&B. Truffaut iPhone actually, stumptown organic before they sold out biodiesel next level sustainable. Offal farm-to-table quinoa shoreditch organic la croix, YOLO air plant etsy. Aesthetic enamel pin taiyaki pok pok. Mlkshk listicle keytar vexillologist. Franzen thundercats ugh affogato pug, tilde deep v meditation pork belly taxidermy synth. Adaptogen echo park hell of squid affogato vaporware banjo af. Vice af portland venmo tumeric gluten-free cardigan pork belly tattooed."
 	address = Faker::Address.street_address + " " + Faker::Address.city
 	new_kitchen = Kitchen.new(user: user, price: price, address: address, title: title, description: description )
-	new_kitchen.picture = image_url;
+	new_kitchen.remote_picture_url = image_path;
 	if new_kitchen.save
 		puts "#{new_kitchen.title} has been created and saved"
 	end
