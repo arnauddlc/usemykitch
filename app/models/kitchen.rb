d76189f2113d95cade382f2631ae5235444294db
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-02-23 12:47:58 +0800

    added destroy dependencies of bookings to kitchen model

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -2,7 +2,7 @@ class Kitchen < ApplicationRecord
   mount_uploader :picture, PictureUploader
   
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   # has_many :users, through: :bookings
 
   validates :title, presence: true
