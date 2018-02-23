54b2301ff8404e988972e98d40ee64dcbd36f8a4
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-02-23 14:12:59 +0800

    added code to include message if user has already booked the kitchen

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -3,10 +3,14 @@ class Kitchen < ApplicationRecord
   
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  # has_many :users, through: :bookings
+  has_many :users, through: :bookings
 
   validates :title, presence: true
   validates :address, presence: true
   validates :price, presence: true
   validates :user_id, presence: true
+
+  def booked_before?(user)
+  	self.users.include?(user)
+  end
 end
