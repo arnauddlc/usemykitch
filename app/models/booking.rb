5d2f44ef2f2be75edbb62f7740ab241d60490979
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-02-26 12:03:59 +0800

    added validation methods in booking model- need to change controller to display errors

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,23 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :kitchen
 
-  validates :start_date, :end_date, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   validates :user_id, presence: true
   validates :kitchen_id, presence: true
+
+  validate :start_must_be_today_or_after, :end_must_be_after_start_date
   # scope :received_bookings, -> { .where('kitchen_id = ?', ) }
+
+  def start_must_be_today_or_after
+  	if start_date.present? && start_date < Date.today
+  		errors.add(:start_date, "cannot be in the past")
+  	end
+  end
+
+  def end_must_be_after_start_date
+  	if end_date.present? && end_date <= start_date
+  		errors.add(:end_date, "must be after the start date")
+  	end
+  end
 end
