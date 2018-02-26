3f2025cb41b3594fa29eb3d40f5bae9a1bc89124
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-02-26 13:03:11 +0800

    added simple route and process, now to add a confirmation modal

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   root to: 'kitchens#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :kitchens do
-    resources :bookings, only: [ :create] do
-      member do
-        post 'save'
-      end
-    end
+    resources :bookings, only: [ :create]
   end
 
   resources :bookings, only: [:destroy] do
