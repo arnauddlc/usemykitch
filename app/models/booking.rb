edcfceedb23d4804d3f80dda9c995678ca53a544
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-02-26 12:18:31 +0800

    added user validation in model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,7 @@ class Booking < ApplicationRecord
   validates :user_id, presence: true
   validates :kitchen_id, presence: true
 
-  validate :start_must_be_today_or_after, :end_must_be_after_start_date
+  validate :start_must_be_today_or_after, :end_must_be_after_start_date, :user_must_not_be_owner
   # scope :received_bookings, -> { .where('kitchen_id = ?', ) }
 
   def start_must_be_today_or_after
@@ -19,6 +19,12 @@ class Booking < ApplicationRecord
   def end_must_be_after_start_date
   	if end_date.present? && end_date <= start_date
   		errors.add(:end_date, "must be after the start date")
+  	end
+  end
+
+  def user_must_not_be_owner
+  	if self.kitchen.user == self.user
+  		errors.add(:user_id, "you cannot book your own kitchen!")
   	end
   end
 end
